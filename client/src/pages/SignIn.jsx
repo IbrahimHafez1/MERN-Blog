430c9afac9a8b58138fb89f98959d9f284d00ba6
Ibrahim Hafez <ibrahimhafez1@yahoo.com>
2024-02-21 15:31:46 +0200

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,111 @@
+import { useState } from "react";
+import { Button, Label, TextInput, Alert, Spinner } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      Sign-In
+    <div className="min-h-screen mt-20">
+      <div className="max-w-3xl mx-auto p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-3xl font-semibold text-center mb-6">Sign Up</h1>
+        <div className="flex flex-col md:flex-row md:items-center md:gap-5">
+          <div className="md:w-1/2">
+            <Link
+              to="/"
+              className="block text-center mb-8 text-4xl font-semibold text-indigo-500"
+            >
+              <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg px-2 py-1 text-white">
+                Ibrahim's
+              </span>{" "}
+              Blog
+            </Link>
+            <p className="text-sm text-gray-600">
+              You can sign in with your email and password or with Google.
+            </p>
+          </div>
+          <div className="md:w-1/2">
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+              <div>
+                <Label value="Your email" />
+                <TextInput
+                  type="email"
+                  placeholder="name@email.com"
+                  id="email"
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <Label value="Your password" />
+                <TextInput
+                  type="password"
+                  placeholder="*******"
+                  id="password"
+                  onChange={handleChange}
+                />
+              </div>
+              <Button
+                gradientDuoTone="purpleToPink"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <Spinner size="sm" />{" "}
+                    <span className="pl-3">Loading...</span>
+                  </>
+                ) : (
+                  "Sign In"
+                )}
+              </Button>
+            </form>
+            <div className="mt-4 text-center">
+              <span>Don't have an account?</span>
+              <Link to="/sign-up" className="text-blue-500">
+                Sign up
+              </Link>
+            </div>
+            {errorMessage && (
+              <Alert className="mt-5" color="failure">
+                {errorMessage}
+              </Alert>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
