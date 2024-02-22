807e2072119bf704ea2d0e2d43b5f7be77cf45d7
Ibrahim Hafez <ibrahimhafez1@yahoo.com>
2024-02-22 18:11:16 +0200

    complete user image upload functionality

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -25,6 +25,7 @@ export default function DashSideBar() {
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div"
             >
               Profile
             </Sidebar.Item>
